b2c8d07dd7b47b3856e19f784729e0a3d038350b
Siddharth Pathak <67097391+sid993100@users.noreply.github.com>
2023-02-18 20:58:55 +0530

    create-custom-hooks

--- src/components/CreatePost.js
+++ src/components/CreatePost.js
@@ -1,11 +1,12 @@
 import { useState } from 'react';
 import { firestore } from '../firebase';
+import {useFormInput} from '../hooks';
 
 
 function CreatePost() {
-  const [title, setTitle] = useState();
-  const [subTitle, setSubTitle] = useState();
-  const [content, setContent] = useState();
+  const title = useFormInput('');
+  const subTitle = useFormInput('');
+  const content = useFormInput('');
 
   function handleSubmit(e) {
     e.preventDefault();
@@ -16,9 +17,9 @@ function CreatePost() {
 
     //posting data to firebase
   firestore.collection('posts').add({
-    title,
-    content,
-    subTitle,
+    title:title.value,
+    content:content.value,
+    subTitle:subTitle.value,
     createdAt: new Date(),
   });
   }
@@ -31,23 +32,17 @@ function CreatePost() {
       <form onSubmit={handleSubmit}>
         <div className="form-field">
           <label>Title</label>
-          <input value={title} onChange={(e) => setTitle(e.target.value)} />
+          <input {...title} />
         </div>
 
         <div className="form-field">
           <label>Sub Title</label>
-          <input
-            value={subTitle}
-            onChange={(e) => setSubTitle(e.target.value)}
-          />
+          <input {...subTitle} />
         </div>
 
         <div className="form-field">
           <label>Content</label>
-          <textarea
-            value={content}
-            onChange={(e) => setContent(e.target.value)}
-          ></textarea>
+          <textarea {...content} ></textarea>
         </div>
 
         <button className="create-post-btn">Create Post</button>
--- src/hooks.js
+++ src/hooks.js
@@ -1,6 +1,8 @@
+import {useState} from 'react';
+
 //here we create a custom hooks----------and custom hooks is start form use-->like
 export function useFormInput(initialValue){
-    const[value, setValue] = useState();
+    const[value, setValue] = useState(initialValue);
 
     function handleChange(e){
          setValue(e.target.value);
